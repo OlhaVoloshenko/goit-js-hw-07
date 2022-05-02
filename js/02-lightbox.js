b72ad4970af9380535052eb8adede3d2e35d6c74
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-05-02 18:42:29 +0300

    HW #1B
    HW #1B

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const galleryImg = createGalerryAtt(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', galleryImg);
+//galleryList.addEventListener('click', galleryListModal);
+
+function createGalerryAtt(galleryItems){
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a> `;
+    }).join('');
+};
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250} );
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
